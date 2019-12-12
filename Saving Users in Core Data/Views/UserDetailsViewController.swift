8b07d901702004297c076f234d7f20652c6afefc
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-12-12 14:38:54 +0200

    feat: Added UserDetailsVC and linked to storyboard

---
+++ Saving Users in Core Data/Views/UserDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UserDetailsViewController.swift
+//  Saving Users in Core Data
+//
+//  Created by Rinalds Domanovs on 12/12/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
